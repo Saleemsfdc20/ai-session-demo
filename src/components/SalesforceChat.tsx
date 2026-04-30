deb31fba4fe824dfb4681c13af516632e05a7545
saleem2001 <saleemshaikhsf@gmail.com>
2026-04-30 06:06:05 +0530

    added the new code snippet

--- src/components/SalesforceChat.tsx
+++ src/components/SalesforceChat.tsx
@@ -1,40 +1,41 @@
 "use client";
 
-import Script from "next/script";
+import { useEffect } from "react";
 
 export function SalesforceChat() {
-  return (
-    <>
-      <Script id="sf-embedded-messaging" strategy="afterInteractive">
-        {`
-          function initEmbeddedMessaging() {
-            try {
-              embeddedservice_bootstrap.settings.language = 'en_US';
+  useEffect(() => {
+    if (typeof window === "undefined") return;
 
-              embeddedservice_bootstrap.init(
-                '00DgK00000KnrZ7',
-                'Website_Chat_Bot',
-                'https://orgfarm-2f0187d65f-dev-ed.develop.my.site.com/ESWWebsiteChatBot1777476051546',
-                {
-                  scrt2URL: 'https://orgfarm-2f0187d65f-dev-ed.develop.my.salesforce-scrt.com'
-                }
-              );
-            } catch (err) {
-              console.error('Error loading Embedded Messaging: ', err);
+    if (document.getElementById("sf-embedded-messaging-script")) {
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.id = "sf-embedded-messaging-script";
+    script.src = "https://orgfarm-2f0187d65f-dev-ed.develop.my.site.com/ESWWebsiteChatBot1777476051546/assets/js/bootstrap.min.js";
+    script.async = true;
+
+    script.onload = () => {
+      try {
+        const win = window as any;
+        if (win.embeddedservice_bootstrap) {
+          win.embeddedservice_bootstrap.settings.language = "en_US";
+          win.embeddedservice_bootstrap.init(
+            "00DgK00000KnrZ7",
+            "Website_Chat_Bot",
+            "https://orgfarm-2f0187d65f-dev-ed.develop.my.site.com/ESWWebsiteChatBot1777476051546",
+            {
+              scrt2URL: "https://orgfarm-2f0187d65f-dev-ed.develop.my.salesforce-scrt.com",
             }
-          };
-          window.initEmbeddedMessaging = initEmbeddedMessaging;
-        `}
-      </Script>
-      <Script 
-        src="https://orgfarm-2f0187d65f-dev-ed.develop.my.site.com/ESWWebsiteChatBot1777476051546/assets/js/bootstrap.min.js" 
-        strategy="afterInteractive"
-        onLoad={() => {
-          if (typeof window !== 'undefined' && (window as any).initEmbeddedMessaging) {
-            (window as any).initEmbeddedMessaging();
-          }
-        }}
-      />
-    </>
-  );
+          );
+        }
+      } catch (err) {
+        console.error("Error loading Embedded Messaging: ", err);
+      }
+    };
+
+    document.body.appendChild(script);
+  }, []);
+
+  return null;
 }
